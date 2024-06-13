707f33183ce2138d840a4baed3ac86a6edd51183
nicolabalzano <nicolab714@gmail.com>
2024-06-13 12:22:06 +0200

    fix table of related attack patterns

--- webInterface/src/pages/threat_show/attack/Attack.jsx
+++ webInterface/src/pages/threat_show/attack/Attack.jsx
@@ -6,7 +6,7 @@ const Attack = () => {
     const primaryInfo = ['Name', 'Type', 'Description'];
     const infoForCardView = ['ID', 'Domains', 'Mitre Kill Chain phases', 'Kill Chain phases', 'Platforms', 'Deprecated']
     const otherImportantInfo = ['Detection suggestions', 'Mitigations', 'Procedure examples', 'Campaigns that exploit this attack pattern', 'Targeted assets',
-        'Attacks patterns that can lead to this', 'Attacks patterns that may occur after this', 'All related attack patterns'];
+        'Attack patterns that can lead to this', 'Attack patterns that may occur after this', 'All related attack patterns'];
 
     return (
         <Threat primaryInfo={primaryInfo} infoForCardView={infoForCardView} otherImportantInfo={otherImportantInfo} fetchDataFunction={fetchDataAttackAPI}/>
