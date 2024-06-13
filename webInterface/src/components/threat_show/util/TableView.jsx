60138dcdc049e309505f0a5b00908ea70d8c64e0
nicolabalzano <nicolab714@gmail.com>
2024-06-14 01:16:57 +0200

    update TabelView.jsx

--- webInterface/src/components/threat_show/util/TableView.jsx
+++ webInterface/src/components/threat_show/util/TableView.jsx
@@ -1,7 +1,7 @@
-import {useEffect} from "react";
-import {useState} from "react";
-import {useLocation} from "react-router-dom";
-import {navigateToThreats} from "../../handle_routing_threats/HandleRoutingThreats";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { navigateToThreats } from "../../handle_routing_threats/HandleRoutingThreats";
 import('../../../scss/util.scss')
 
 const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
@@ -13,14 +13,29 @@ const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
     const location = useLocation();
     let alreadySelected = location.state ? location.state.alreadySelected : null;
 
+    var parentIdForRendering = '*****';
 
     useEffect(() => {
         const root_element = document.getElementById('root');
         root_element.classList.add('scrollbar')
         if (alreadySelected)
             setSelectedAt(alreadySelected);
-            alreadySelected = null;
+        alreadySelected = null;
     }, []);
+
+    function isChild(idChild) {
+        if (idChild.includes(parentIdForRendering)) {
+            return true;
+        }
+        parentIdForRendering = idChild;
+        return false;
+    }
+    function nextIdIsParent(idParent, idChild) {
+        if (idParent.includes(idChild)) {
+            return true;
+        }
+        return false;
+    }
 
     function generateRandomColor() {
         const red = Math.floor(Math.random() * 256);
@@ -29,8 +44,8 @@ const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
         return `rgb(${red}, ${green}, ${blue})`;
     }
 
-    function getColorForSource(source){
-        if (source in colorMapSource){
+    function getColorForSource(source) {
+        if (source in colorMapSource) {
             return colorMapSource[source];
         } else {
             colorMapSource[source] = generateRandomColor();
@@ -40,28 +55,64 @@ const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
     }
 
     function formatContentId(content_id) {
-        content_id = content_id.replace(/\./g, '\\.');       
+        content_id = content_id.replace(/\./g, '\\.');
         content_id = content_id.replace(/\&/g, '\\&');
         return content_id;
     }
 
-    function handleSelectedCell(event) {
-        const content = event.target;
-        const elements = document.querySelectorAll('#' + formatContentId(content.id));
+    function handleSelectedCell(contentId) {
+        const elements = document.querySelectorAll('#' + formatContentId(contentId));
+        console.log(selectedAt);
 
-        if (selectedAt && selectedAt.includes(content.id)) {
-            setSelectedAt(selectedAt.filter(at => at !== content.id));
+        if (selectedAt && selectedAt.includes(contentId)) {
+            setSelectedAt(selectedAt.filter(at => at !== contentId));
             elements.forEach(element => {
-                element.classList.remove("bg-primary-opacity");
+                {/* Avoid to change the color of the caret icon */}
+                if(element.tagName !== 'I'){ 
+                    element.classList.remove("bg-primary-opacity");
+                }
             });
         }
         else {
-            setSelectedAt([...selectedAt, content.id]);
+            setSelectedAt([...selectedAt, contentId]);
             elements.forEach(element => {
-                element.classList.add("bg-primary-opacity");
+                {/* Avoid to change the color of the caret icon */}
+                if(element.tagName !== 'I'){
+                    element.classList.add("bg-primary-opacity");
+                }
             });
         }
 
+    }
+
+    function handleDropdown(event, parentId) {
+        const contentId = parentId;
+        const allElements = document.querySelectorAll('*');
+        let i = event.target.closest('i')
+
+        allElements.forEach(element => {
+            if ( !element.id.endsWith(contentId) && element.id.includes(contentId)) {
+                if (element.classList.contains('d-none')) {
+                    element.classList.remove('d-none');
+                    i.classList.remove('bi-caret-down-fill');
+                    i.classList.add('bi-caret-up-fill');
+                }
+                else {
+                    element.classList.add('d-none');
+                    i.classList.remove('bi-caret-up-fill');
+                    i.classList.add('bi-caret-down-fill');
+                }
+            }
+        });
+    }
+
+    function isClickedDropdown(dropdown_id) {
+        const dropdown = document.getElementById(dropdown_id);
+
+        if (dropdown && dropdown.classList.contains('bi-caret-up-fill')) {
+            return true;
+        }
+        return false;
     }
 
     useEffect(() => {
@@ -69,8 +120,8 @@ const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
 
     return (
         <div className="row d-flex justify-content-center">
-            <div className="mt-3 mb-4  text-center lead">You can select the attack patterns identified through
-                your analysis to find out if the attacks you have suffered can be linked to a known Threat Agets/Groups.
+            <div className="mt-3 mb-4 text-center lead">You can select the attack patterns identified through
+                your analysis to find out if the attacks you have suffered can be linked to a known Threat Agents/Groups.
             </div>
 
             {Object.keys(colorMapSource).length > 0 && (
@@ -88,7 +139,7 @@ const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
                                 </span>
                                 <span
                                     className="d-inline-block ms-1"
-                                    style={{background: value, width: '10px', height: '10px'}}
+                                    style={{ background: value, width: '10px', height: '10px' }}
                                 ></span>
                             </div>
                         ))}
@@ -104,39 +155,72 @@ const TableView = ({ infoList, selectedAt, setSelectedAt }) => {
                                 <span className="fw-semibold fs-6">{key}</span>
                             </p>
                             {Array.isArray(value) ? (
-                                value.sort((a,b)=> a['ID'].localeCompare(b['ID'])).map((subItem, subIndex) => (
+                                value.sort((a, b) => a['ID'].localeCompare(b['ID'])).map((subItem, subIndex) => {
+
+                                    {/* If the attack pattern is a child, hidden that to show when client click on caret down */}
+                                    const isChildPattern=isChild(subItem['ID'])
+                                    var classesForChild='ms-0'
+                                    if(isChildPattern && isClickedDropdown(key + '_' + parentIdForRendering)){
+                                        classesForChild='ms-3'
+                                    }
+                                    else if (isChildPattern && !selectedAt.includes(key.replaceAll(' ', '_') + '__' + subItem.ID)){
+                                        classesForChild='ms-3 d-none'
+                                    }           
+                                    else if(isChildPattern){
+                                        classesForChild='ms-3'
+                                    }
+
+                                    return (
                                     <div
                                         key={subIndex}
                                         className={
                                             (selectedAt.includes(key.replaceAll(' ', '_') + '__' + subItem.ID)
-                                                ? 'm-0 border-bottom border-start border-end border-secondary py-2 bg-primary-opacity'
-                                                : 'm-0 border-bottom border-start border-end border-secondary py-2')}
-                                        style={{fontSize: '12px'}}
+                                                ? 'm-0 border-bottom border-top border-start border-end border-secondary bg-primary-opacity d-flex align-items-center py-2'
+                                                : 'm-0 border-bottom border-top border-start border-end border-secondary d-flex align-items-center py-2') + ' ' + classesForChild}
+                                        style={{ fontSize: '12px' }}
                                         id={key.replaceAll(' ', '_') + '__' + subItem.ID}
-                                        role="button"
-                                        onClick={(e) => handleSelectedCell(e)}
                                     >
-                                        {subItem.hasOwnProperty('Attack name') ? subItem['Attack name'] : subItem['Name']}
-                                        <p
-                                            className="text-decoration-underline link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover m-0"
+                                        <div 
+                                            style={{ flex: '90%' }} 
                                             role="button"
-                                            onClick={() => navigateToThreats(subItem['ID'])}
-                                        >
-                                            {subItem['ID']}
-                                        </p>
-                                        <div className="d-flex justify-content-end me-1">
-                                            {subItem.hasOwnProperty('Relationship Source ID') && (
-                                                <div
-                                                    style={{
-                                                        background: getColorForSource(subItem['Relationship Source ID']),
-                                                        width: '10px',
-                                                        height: '10px'
-                                                    }}
-                                                ></div>
-                                            )}
+                                            onClick={(e) => handleSelectedCell(key.replaceAll(' ', '_') + '__' + subItem.ID)}
+                                        > 
+                                            <div onClick={(e) => { e.currentTarget.parentNode.click() }}> {/* Click on the row perform click on parent to select cell*/}
+                                                {subItem.hasOwnProperty('Attack name') ? subItem['Attack name'] : subItem['Name']}
+                                            </div>
+                                            <p
+                                                className="text-decoration-underline link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover m-0"
+                                                role="button"
+                                                onClick={() => navigateToThreats(subItem['ID'])}
+                                            >
+                                                {subItem['ID']}
+                                            </p>
+
+                                            {/* If there is a relationship, put a color for that */}
+                                            <div className="d-flex justify-content-end me-1">
+                                                {subItem.hasOwnProperty('Relationship Source ID') && (
+                                                    <div
+                                                        style={{
+                                                            background: getColorForSource(subItem['Relationship Source ID']),
+                                                            width: '10px',
+                                                            height: '10px'
+                                                        }}
+                                                    ></div>
+                                                )}
+                                            </div>
                                         </div>
+                                        
+                                        {/* Exapand attack patterns for child */}
+                                        {
+                                            !isChildPattern && value.length-1 > subIndex && nextIdIsParent(value[subIndex+1]['ID'], subItem['ID'] ) && (
+                                                <div style={{ flex: '10%' }} onClick={(e)=>{handleDropdown(e, key.replaceAll(' ', '_') + '__' + subItem.ID)}}>
+                                                    <i id={key + '_' + parentIdForRendering} className="bi bi-caret-down-fill text-secondary" ></i>
+                                                </div>
+                                            )
+                                        }
                                     </div>
-                                ))
+                                    )
+                                })
                             ) : null}
                         </div>
                     ))
